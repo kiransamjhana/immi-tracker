f89e69fe5bfcaf3d0999c33e163767c9da0b7a9a
kiransamjhana <kiranmanandhar956@gmail.com>
2024-03-25 18:46:57 +1100

    landing page building

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Features from "./components/Features";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="flex min-h-screen flex-col  ">
+      <Navbar />
+      <HeroSection />
+      <Features />
     </main>
   );
 }
